254b002af5146214425e84ae711b3c0825b4e0fe
mluc <my.luc@opcity.com>
2020-01-05 19:15:42 -0600

    before circleci

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=myluc/udacity-devops:proj4
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run --generator=run-pod/v1 my-app --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward my-app 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=myluc/udacity-devops:proj4
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username myluc --password 353162b3-0a86-4354-bfc1-935913c23a54
+docker tag project4 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
